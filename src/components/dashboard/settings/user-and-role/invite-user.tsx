b1c4a821691afd8eafc933373525d17682dc0fee
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2024-11-20 14:48:46 +0100

    UPDATE: Add new branch implementation

--- src/components/dashboard/settings/user-and-role/invite-user.tsx
+++ src/components/dashboard/settings/user-and-role/invite-user.tsx
@@ -1,16 +1,16 @@
 "use client";
 
+import { useInviteUserModalAction } from "@/store/use-invite-user-modal";
 import CustomButton from "../../custom-button";
 import { InviteUserModalForm } from "./invite-user-modal";
-import { useInviteUserModalAction } from "@/store/use-invite-user-modal";
 
 export function InviteUser() {
   const { open, onOpenChange } = useInviteUserModalAction();
   return (
     <>
       <InviteUserModalForm />
-      <div className="flex max-lg:flex-col justify-between">
-        <div className="h-[88px] pb-px flex-col justify-start items-start gap-5 inline-flex">
+      <div className="flex max-lg:flex-col gap-4 justify-between">
+        <div className=" pb-px flex-col justify-start items-start gap-5 inline-flex">
           <div className="self-stretch justify-start items-start gap-4 inline-flex">
             <div className="grow shrink basis-0 flex-col justify-start items-start gap-2 inline-flex">
               <div className="self-stretch text-slate-700 text-lg font-semibold leading-7">
@@ -31,7 +31,7 @@ export function InviteUser() {
         </div>
         <CustomButton
           onClick={() => onOpenChange(true)}
-          className="bg-primary-100 h-fit text-white"
+          className="bg-primary-100 h-fit w-fit text-white"
           label="invite members"
         />
       </div>
