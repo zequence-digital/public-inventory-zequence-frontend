2cdf79f096cf30a8492fc68d435b645699265c3e
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2024-11-29 15:58:35 +0100

    UPDATE: Add calender filter

--- src/components/dashboard/side-nav.tsx
+++ src/components/dashboard/side-nav.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import NavLinks from "@/components/dashboard/nav-links";
-import { SubLinks } from "./sub-links";
-import { UserMenu } from "../user-menu/user-menu";
 import { useMediaQuery } from "usehooks-ts";
+import { UserMenu } from "../user-menu/user-menu";
+import { SubLinks } from "./sub-links";
 
 export function SideNav() {
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -12,18 +12,18 @@ export function SideNav() {
       {isMobile ? (
         <div className="h-fit fixed top-0 w-full bg-gray-50 shadow-lg z-50">
           <UserMenu />
-          <main className="flex items-start justify-between">
+          <div className="flex items-start justify-between">
             <NavLinks />
             <SubLinks />
-          </main>
+          </div>
         </div>
       ) : (
         <aside className="flex md:w-64 w-fit h-full flex-col px-3 py-4 md:pt-10 pt-0 md:px-2">
-          <div>
+          <>
             <UserMenu />
 
             <NavLinks />
-          </div>
+          </>
           <div className=" md:h-full w-full md:items-end md:mt-auto flex">
             <SubLinks />
           </div>
